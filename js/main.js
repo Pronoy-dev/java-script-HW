fe1c8d6747fd57d90c54db7fde0b67490bef7f3f
Pronoy-dev <propronoy3@gmail.com>
2024-05-15 15:10:56 +0600

    popup practice done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-// popup
+const allImg = [...document.querySelectorAll(".image")];
+const popUp = document.querySelector(".popUp");
+const closeBtn = document.querySelector(".close-btn");
+const bigImg = document.querySelector(".big_img");
+
+allImg.map((singleImg) => {
+  singleImg.addEventListener("click", (event) => {
+    imgChanger(event.target.src);
+    popUp.classList.add("active");
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  popUp.classList.remove("active");
+});
+
+function imgChanger(ImgUrl) {
+  bigImgUrl = ImgUrl.split("/").splice(9).join("/");
+  bigImg.src = bigImgUrl;
+}
