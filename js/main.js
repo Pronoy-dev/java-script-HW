43ef36816d0f71fba90c726803a56af2193cefb0
Pronoy-dev <propronoy3@gmail.com>
2024-05-26 20:08:45 +0600

    callback and promise practice going

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 function work() {
   const fetchPromise = new Promise((resolve, reject) => {
-    fetch("https://dummyjson.com/products").then((Response) => {
-      console.log(Response);
-    });
+    fetch("https://dummyjson.com/products")
+      .then((Response) => {
+        return Response.json();
+      })
+      .then((returnData) => {
+        return returnData.products;
+      })
+      .then((finalProduct) => {
+        resolve(finalProduct);
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
   return fetchPromise;
 }
 
-work();
+work()
+  .then((data) => {
+    return data.splice(0, 10);
+  })
+  .then((data) => {
+    console.log(data);
+  });
