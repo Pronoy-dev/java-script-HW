c542edc58b720050606282d8a054aae614d6a29c
Pronoy-dev <propronoy3@gmail.com>
2024-05-29 00:59:20 +0600

    api hit done

--- js/main.js
+++ js/main.js
@@ -1,26 +1,48 @@
-function work() {
-  const fetchPromise = new Promise((resolve, reject) => {
-    fetch("https://dummyjson.com/products")
+const productWrapper = document.querySelector(".productWrapper");
+
+function fetcherProduct(api) {
+  const myPromise = new Promise((resolve, reject) => {
+    fetch(api)
       .then((Response) => {
         return Response.json();
       })
-      .then((returnData) => {
-        return returnData.products;
-      })
-      .then((finalProduct) => {
-        resolve(finalProduct);
+      .then((allData) => {
+        resolve(allData.products);
       })
       .catch((err) => {
         reject(err);
       });
   });
-  return fetchPromise;
+  return myPromise;
 }
 
-work()
-  .then((data) => {
-    return data.splice(0, 10);
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// ====== display function =======
+
+function displayAllProducts(allProducts) {
+  let allProductsHtml = ``;
+  for (let i = 0; i < allProducts.length; i++) {
+    allProductsHtml += `<div class="eachProduct">
+    <h1 class="title">${allProducts[i].title}</h1>
+    <img
+      src=${allProducts[i].thumbnail}
+      alt=${allProducts[i].thumbnail}
+      class="image"
+    />
+    <p>price : ${allProducts[i].price}</p>
+    <p class="description">
+    ${allProducts[i].description}
+    </p>
+  </div>`;
+  }
+
+  productWrapper.innerHTML = allProductsHtml;
+}
+
+// ====== display function =======
+
+RmyPromise = fetcherProduct("https://dummyjson.com/products");
+RmyPromise.then((data) => {
+  displayAllProducts(data);
+}).catch((err) => {
+  console.log(err);
+});
